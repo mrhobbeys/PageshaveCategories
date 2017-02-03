7fdec021bcc5fbc92ca663dce5bdfea822e9c6a3
mrhobbeys <mrhobbeys@gmail.com>
2017-02-03 09:28:58 -0600

    Create pageshavecategories.php
    Started file by creating header

---
+++ pageshavecategories.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Pages Have Categories
+Plugin URI:  https://hektechnologies.com/
+Description: Basic WordPress Plugin to create category for pages
+Version:     0.1
+Author:      Spencer Heckathorn
+Author URI:  https://spencerheckathorn.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// nothing yet
